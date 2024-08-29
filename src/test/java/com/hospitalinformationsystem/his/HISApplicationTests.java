3c878a49b6b6c95ad99bad7ffaaf471906a398c9
arkohaddoebenezer <arkohaddoebenezer21@gmail.com>
2024-08-29 14:36:22 +0000

    initial setup

---
+++ src/test/java/com/hospitalinformationsystem/his/HISApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalinformationsystem.his;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HISApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
