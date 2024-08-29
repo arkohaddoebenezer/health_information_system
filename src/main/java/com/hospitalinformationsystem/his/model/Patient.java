e0fdf2ff3cd924d2146012b28f59c57d7d0ca0ee
arkohaddoebenezer <arkohaddoebenezer21@gmail.com>
2024-08-29 21:29:10 +0000

    feat: generated entity setters and getters

--- src/main/java/com/hospitalinformationsystem/his/model/Patient.java
+++ src/main/java/com/hospitalinformationsystem/his/model/Patient.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class Patient {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long patientNumber;
 
     @Embedded
--- src/main/java/com/hospitalinformationsystem/his/model/Ward.java
+++ src/main/java/com/hospitalinformationsystem/his/model/Ward.java
@@ -1,71 +1,72 @@
-package com.hospitalinformationsystem.his.model;
+ package com.hospitalinformationsystem.his.model;
 
-import com.mysql.cj.log.Log;
-import jakarta.persistence.*;
+ import com.mysql.cj.log.Log;
+ import jakarta.persistence.*;
 
-@Entity
-public class Ward {
+ @Entity
+ public class Ward{
 
-    @Id
-    private String wardID;
-    private String name;
-    private Long wardNumber;
-    private Long numberOfBeds;
+     @Id
+     private String wardID;
 
-    @ManyToOne
-    @JoinColumn(name = "department_code", referencedColumnName = "departmentCode")
-    private Department department;
+     private String name;
+     private Long wardNumber;
+     private Long numberOfBeds;
 
-    @OneToOne
-    @JoinColumn(name = "supervisor_id", referencedColumnName = "nurseId")
-    private Nurse supervisor;
+     @ManyToOne
+     @JoinColumn(name = "department_code", referencedColumnName = "departmentCode")
+     private Department department;
+
+     @OneToOne
+     @JoinColumn(name = "supervisor_id", referencedColumnName = "EmployeeNumber")
+     private Nurse supervisor;
 
 
-    public String getWardID() {
-        return wardID;
-    }
+     public String getWardID() {
+         return wardID;
+     }
 
-    public void setWardID(Long departmentId, Long wardNumber) {
-        this.wardID = ""+departmentId+wardNumber;
-    }
+     public void setWardID(Long departmentId, Long wardNumber) {
+         this.wardID = ""+departmentId+wardNumber;
+     }
 
-    public String getName() {
-        return name;
-    }
+     public String getName() {
+         return name;
+     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+     public void setName(String name) {
+         this.name = name;
+     }
 
-    public Long getWardNumber() {
-        return wardNumber;
-    }
+     public Long getWardNumber() {
+         return wardNumber;
+     }
 
-    public void setWardNumber(Long wardNumber) {
-        this.wardNumber = wardNumber;
-    }
+     public void setWardNumber(Long wardNumber) {
+         this.wardNumber = wardNumber;
+     }
 
-    public Long getNumberOfBeds() {
-        return numberOfBeds;
-    }
+     public Long getNumberOfBeds() {
+         return numberOfBeds;
+     }
 
-    public void setNumberOfBeds(Long numberOfBeds) {
-        this.numberOfBeds = numberOfBeds;
-    }
+     public void setNumberOfBeds(Long numberOfBeds) {
+         this.numberOfBeds = numberOfBeds;
+     }
 
-    public Department getDepartment() {
-        return department;
-    }
+     public Department getDepartment() {
+         return department;
+     }
 
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
+     public void setDepartment(Department department) {
+         this.department = department;
+     }
 
-    public Nurse getSupervisor() {
-        return supervisor;
-    }
+     public Nurse getSupervisor() {
+         return supervisor;
+     }
 
-    public void setSupervisor(Nurse supervisor) {
-        this.supervisor = supervisor;
-    }
-}
+     public void setSupervisor(Nurse supervisor) {
+         this.supervisor = supervisor;
+     }
+ }
