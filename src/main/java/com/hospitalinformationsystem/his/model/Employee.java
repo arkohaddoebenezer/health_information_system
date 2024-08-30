8c3709036ea00adfb51a1f7cceee0724ee35c576
arkohaddoebenezer <arkohaddoebenezer21@gmail.com>
2024-08-30 11:00:03 +0000

    chore: added constraints and validations

--- src/main/java/com/hospitalinformationsystem/his/model/Employee.java
+++ src/main/java/com/hospitalinformationsystem/his/model/Employee.java
@@ -1,23 +1,70 @@
  package com.hospitalinformationsystem.his.model;
 
- import jakarta.persistence.Embedded;
- import jakarta.persistence.Id;
- import jakarta.persistence.MappedSuperclass;
+ import jakarta.annotation.Nullable;
+ import jakarta.persistence.*;
+ import jakarta.validation.constraints.NotBlank;
+ import jakarta.validation.constraints.NotNull;
 
  @MappedSuperclass
  public abstract class Employee {
      @Id
-     String EmployeeNumber;
+     @NotNull
+     @NotBlank
+     @Column(unique = true)
+     String employeeNumber;
 
-     @Embedded
-     private Contact contact;
+     @NotNull(message = "first name should be provided")
+     @NotBlank(message = "first name should not be empty")
+     private String firstname;
 
+     @NotNull(message = "surname name should be provided")
+     @NotBlank(message = "surname name should not be empty")
+     private String surname;
+
+     @Nullable
+     private String telephoneNumber;
+
+     @Nullable
+     private String address;
 
      public String getEmployeeNumber() {
-         return EmployeeNumber;
+         return employeeNumber;
      }
 
      public void setEmployeeNumber(String EmployeeNumber) {
-         this.EmployeeNumber = EmployeeNumber;
+         this.employeeNumber = EmployeeNumber;
+     }
+
+
+     public String getFirstname() {
+         return firstname;
+     }
+
+     public void setFirstname(String firstname) {
+         this.firstname = firstname;
+     }
+
+     public String getSurname() {
+         return surname;
+     }
+
+     public void setSurname(String surname) {
+         this.surname = surname;
+     }
+
+     public String getTelephoneNumber() {
+         return telephoneNumber;
+     }
+
+     public void setTelephoneNumber(String telephoneNumber) {
+         this.telephoneNumber = telephoneNumber;
+     }
+
+     public String getAddress() {
+         return address;
+     }
+
+     public void setAddress(String address) {
+         this.address = address;
      }
  }
--- src/main/java/com/hospitalinformationsystem/his/model/Patient.java
+++ src/main/java/com/hospitalinformationsystem/his/model/Patient.java
@@ -1,6 +1,9 @@
 package com.hospitalinformationsystem.his.model;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Patient {
@@ -8,9 +11,18 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long patientNumber;
+    @NotNull(message = "first name should be provided")
+    @NotBlank(message = "first name should not be empty")
+    private String firstname;
 
-    @Embedded
-    private Contact contact;
+    @NotNull(message = "surname name should be provided")
+    @NotBlank(message = "surname name should not be empty")
+    private String surname;
+    @Nullable
+    private String telephoneNumber;
+    @Nullable
+    private String address;
+
 
     public Long getNumber() {
         return patientNumber;
@@ -20,11 +32,36 @@ public class Patient {
         this.patientNumber = number;
     }
 
-    public Contact getContact() {
-        return contact;
+
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setContact(Contact contact) {
-        this.contact = contact;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
