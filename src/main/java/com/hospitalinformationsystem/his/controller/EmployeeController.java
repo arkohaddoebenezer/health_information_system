45c03a3e12f515da62583dbef5fc83768fcc25d9
arkohaddoebenezer <arkohaddoebenezer21@gmail.com>
2024-08-30 13:21:43 +0000

    test: employee controller and service test

--- src/main/java/com/hospitalinformationsystem/his/controller/EmployeeController.java
+++ src/main/java/com/hospitalinformationsystem/his/controller/EmployeeController.java
@@ -16,7 +16,6 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
